29a96fd92a1d8ed082bb723aed56fed12e5631bd
UAShoshi <ummeaymonafifha@gmail.com>
2024-06-13 00:39:38 +0600

    project done

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-      origin: ['http://localhost:5173', ''],
+      origin: [
+        // 'http://localhost:5173', 
+        'https://services-by-doctors.web.app',
+      'https://services-by-doctors.firebaseapp.com'],
       credentials: true,
   }),
 )
